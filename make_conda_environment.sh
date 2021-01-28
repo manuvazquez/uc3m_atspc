1fc80ed5319448d8c0cb3e152c434bc13cd90cb4
Manuel A. Vázquez <manuavazquez@gmail.com>
2021-01-28 17:21:04 +0100

    - teaching 	- notebooks 		- tracking.ipynb 			- some improvements (mostly documentation) - make_conda_environment.sh, environment.yml 	- simplified

--- make_conda_environment.sh
+++ make_conda_environment.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
+NAME=at
+MANAGER="mamba"
+
 COLOR="\033[40m\033[32m"
 UNCOLOR="\033[0m"
-
-NAME=at
 
 # only required if "anaconda" is not in the path
 source $HOME/anaconda3/etc/profile.d/conda.sh
 
-# conda create --yes -n $NAME python=3 ipdb bokeh jupyterlab ipywidgets ipycanvas nodejs -c defaults -c conda-forge
-conda create --yes -n $NAME python=3 bokeh jupyterlab ipywidgets ipycanvas nodejs -c defaults -c conda-forge
+$MANAGER create --yes -n $NAME python=3 bokeh jupyterlab ipywidgets ipycanvas -c defaults -c conda-forge
 
 conda activate $NAME
 
-# ipycanvas
-jupyter labextension install @jupyter-widgets/jupyterlab-manager ipycanvas
-
-# bokeh
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
-jupyter labextension install @bokeh/jupyter_bokeh
-
 echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
 
-# the environment is exported into a yaml file
- conda env export --no-builds --from-history -f environment.yml
+# # the environment is exported into a yaml file
+#  conda env export --no-builds --from-history -f environment.yml
