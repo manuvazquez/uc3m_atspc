406f1deb9d9d2cd543a72a71f545d8190ff9dbe8
Manuel A. Vázquez <manuavazquez@gmail.com>
2021-12-27 19:06:58 +0100

    - added "README.md"

---
+++ README.md
@@ -0,0 +1,5 @@
+Interactive material for the [Advanced techniques in signal processing and communications course](https://aplicaciones.uc3m.es/cpa/generaFicha?est=227&anio=2021&plan=171&asig=14315&idioma=2) taught at Carlos III University. You can find most of the theoretical content [here](https://manuvazquez.github.io/advanced-techniques/).
+
+# Interactive jupyter notebooks
+
+- [Tracking](https://mybinder.org/v2/gh/manuvazquez/uc3m_atspc/master?filepath=teaching%2Fnotebooks%2Ftracking.ipynb)
--- make_conda_environment.sh
+++ make_conda_environment.sh
@@ -9,11 +9,6 @@ UNCOLOR="\033[0m"
 # only required if "anaconda" is not in the path
 source $HOME/anaconda3/etc/profile.d/conda.sh
 
-$MANAGER create --yes -n $NAME python=3 bokeh jupyterlab ipywidgets ipycanvas -c defaults -c conda-forge
-
-conda activate $NAME
+$MANAGER create --yes -n $NAME ipdb jupyterlab ipywidgets bokeh ipycanvas -c defaults -c conda-forge
 
 echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
-
-# # the environment is exported into a yaml file
-#  conda env export --no-builds --from-history -f environment.yml
